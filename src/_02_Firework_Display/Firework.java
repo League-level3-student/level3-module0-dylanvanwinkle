b36de22724d21d5934634e3ac90ece557ff6b2dc
dylanvanwinkle <dylanvanwinkle@gmail.com>
2020-06-24 11:29:46 -0700

    ArrayFunIP

--- src/_02_Firework_Display/Firework.java
+++ src/_02_Firework_Display/Firework.java
@@ -1,5 +1,6 @@
 package _02_Firework_Display;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
 
@@ -14,58 +15,63 @@ public class Firework{
 	public static final int GRAVITY = 1;
 	
 	//1. Create an array of Spark objects called sparks. Don't initialize it.
-	
+	Spark[] s;
 	public boolean dead = false;
 	
 	public Firework() {
 		//2. Initialize the Spark array to contain 100 Sparks. 
-Spark[] s = new Spark[100];
+s = new Spark[100];
 		
 		//3. Iterate through the sparks and initialize each one to a new Spark.
 		//   Make each spark start at the middle bottom of the screen.
-	
+launch();
 	}
 	
 	public void launch() {
 		//4. Iterate through the sparks and reset their x and y location
 		//   to their original starting point.
-		
-	}
+		for (int i = 0; i < s.length; i++) {
+			s[i] = new Spark(FireworkDisplay.WIDTH/2,FireworkDisplay.HEIGHT-20);
+		}	}
 	
 	public void updateSparks() {
 		//8. Uncomment this code. See if you can understand what it is doing.
 		
-//		for(int i = 0; i < sparks.length; i++) {
-//
-//			Spark s = sparks[i];
-//			s.x += s.xVelocity;
-//			s.y += s.yVelocity;
-//			s.yVelocity += GRAVITY;
-//			
-//			if(s.yVelocity >= 0 && !s.fire) {
-//				Random r = new Random();
-//				s.xVelocity = r.nextInt(40) - 20;
-//				s.yVelocity += -r.nextInt(10) - 5;
-//				s.size = r.nextInt(7) + 2;
-//				s.fire = true;
-//			}
-//			
-//			if(s.y >= FireworkDisplay.HEIGHT) {
-//				s.dead = true;
-//			}
-//		}
-//		
-//		for(int i = 0; i < sparks.length; i++) {
-//			if(!sparks[i].dead) {
-//				dead = false;
-//				break;
-//			}
-//			dead = true;
-//		}
+		for(int i = 0; i < s.length; i++) {
+
+			Spark s2= s[i];
+			s2.x += s2.xVelocity;
+			s2.y += s2.yVelocity;
+			s2.yVelocity += GRAVITY;
+			
+			if(s2.yVelocity >= 0 && !s2.fire) {
+				Random r = new Random();
+				s2.xVelocity = r.nextInt(40) - 20;
+				s2.yVelocity += -r.nextInt(10) - 5;
+				s2.size = r.nextInt(7) + 2;
+				s2.fire = true;
+			}
+			
+			if(s2.y >= FireworkDisplay.HEIGHT) {
+				s2.dead = true;
+			}
+		}
+		
+		for(int i = 0; i < s.length; i++) {
+			if(!s[i].dead) {
+				dead = false;
+				break;
+			}
+			dead = true;
+		}
 	}
 	
 	public void drawSparks(Graphics g) {
 		//5. Iterate through each spark
+		for (int i = 0; i < s.length; i++) {
+		g.setColor(Color.BLUE);
+		g.fillOval(s[i].x, s[i].y, s[i].size,s[i].size);
+		}
 			//6. Set the color to the spark's color with g.setColor()
 			//7. Fill in an oval using the spark's x, y, and size with g.fillOval()
 	}
--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -3,24 +3,59 @@ package _03_More_Array_Fun;
 import java.util.Random;
 
 public class MoreArrayFun {
-	//1. Create a main method to test the other methods you write.
-	
-	
-	
-	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
-	
-	
-	
-	//3. Write a method that takes an array of Strings and prints all the Strings in the array
-	//   in reverse order.
-	
-	
-	
-	//4. Write a method that takes an array of Strings and prints every other String in the array.
-	
-	
-	//5. Write a method that takes an array of Strings and prints all the Strings in the array
-	//   in a completely random order. Almost every run of the program should result in a different order.
-	
-	
+	// 1. Create a main method to test the other methods you write.
+	public static void main(String[] args) {
+
+	}
+
+	// 2. Write a method that takes an array of Strings and prints all the Strings
+	// in the array.
+	public void string(String[] s) {
+		for (int i = 0; i < s.length; i++) {
+			System.out.println(s[i]);
+		}
+	}
+
+	// 3. Write a method that takes an array of Strings and prints all the Strings
+	// in the array
+	// in reverse order.
+	public void string2(String[] s) {
+		for (int i = s.length; i > s.length; i--) {
+			System.out.println(s[i]);
+		}
+		}
+
+	// 4. Write a method that takes an array of Strings and prints every other
+	// String in the array.
+
+	public void string3(String[] s) {
+		for (int i = 0; i < s.length; i++) {
+			if (i % 2 == 0) {
+				System.out.println(s[i]);
+			}
+		}
+	}
+	// 5. Write a method that takes an array of Strings and prints all the Strings
+	// in the array
+	// in a completely random order. Almost every run of the program should result
+	// in a different order.
+
+	public void string4(String[] s) {
+		boolean c = false;
+		while(!c) {
+			Random r = new Random();
+int ran = r.nextInt(s.length);
+if(s[ran] != null) {
+System.out.println(s[ran]);
+s[ran] = null;
+}
+for (int i = 0; i < s.length; i++) {
+	if(s[i] == null) {
+		c = true;
+	}
+}
+}
+
+
+	}
 }
